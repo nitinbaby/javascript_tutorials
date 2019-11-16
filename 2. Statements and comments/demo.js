479d2de3733ab55c1ebf57b9fe3b18a3352651b2
nitinbaby <tvbnitin@gmail.com>
2019-11-16 22:08:19 +0530

    Statements and comments

---
+++ 2. Statements and comments/demo.js
@@ -0,0 +1,10 @@
+// This is a comment
+document.write("<h1>Hello World1!!</h1>"); // Hello World
+document.write("<h1>Hello World2!!</h1>"); // This is another code snip
+document.write("<h1>Hello World2!!</h1>"); // This is trivial here
+
+/*
+document.write("<h1>Hello World1!!</h1>"); // Hello World
+document.write("<h1>Hello World2!!</h1>"); // This is another code snip
+document.write("<h1>Hello World2!!</h1>");
+*/
