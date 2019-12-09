49c09cc92b0acd34795d196e8b927ed5838ec7a0
TB Nitin <tvbnitin@gmail.com>
2019-12-09 23:35:13 +0530

    Update demo.js

--- 3. Tricks/demo.js
+++ 3. Tricks/demo.js
@@ -1,7 +1,8 @@
 // This is a comment
 document.write("<h1>Hello World1!!</h1>"); // Hello World
 document.write("<h1>Hello World2!!</h1>"); // This is another code snip
-document.write("<h1>Hello World2!!</h1>"); // This is trivial here
+document.write("<h1>Hello World3!!</h1>"); // This is trivial here
+document.write("<h1>Hello World4!!</h1>"); // This is also trivial here
 
 /*
 document.write("<h1>Hello World1!!</h1>"); // Hello World
